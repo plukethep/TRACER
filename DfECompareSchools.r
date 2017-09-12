387ad91f9d02bca098705f660c10bce4ff25d5b5
plukethep <peterejkemp@gmail.com>
2017-09-12 09:26:12 +0100

    Update DfECompareSchools.r

--- DfECompareSchools.r
+++ DfECompareSchools.r
@@ -5,6 +5,7 @@ library(magrittr)
 
 # download 2016 school summary data from: https://www.compare-school-performance.service.gov.uk/
 # https://www.compare-school-performance.service.gov.uk/download-data?currentstep=datatypes&regiontype=all&la=0&downloadYear=2015-2016&datatypes=ks4underlying
+# https://www.compare-school-performance.service.gov.uk/download-data?download=true&regions=0&filters=KS4UNDERLYING&fileformat=xls&year=2015-2016&meta=false
 
 loadEntries <- function(filename){
 
